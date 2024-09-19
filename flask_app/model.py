cd1a8fd30e7ce55b123e1f9c1a1a4098a5059b81
Bertrand Estellon <bertrand.estellon@univ-amu.fr>
2024-09-19 11:50:55 +0200

    TD2 : teams, matches

--- flask_app/model.py
+++ flask_app/model.py
@@ -4,10 +4,19 @@ from flask_app import data
 from flask_app import ranking
 
 
+def dictionary_factory(cursor, row):
+  dictionary = {}
+  for index in range(len(cursor.description)):
+    column_name = cursor.description[index][0]
+    dictionary[column_name] = row[index]
+  return dictionary
+
+
 def connect(database = "database.sqlite"):
   connection = sqlite3.connect(database)
   connection.set_trace_callback(print)
   connection.execute('PRAGMA foreign_keys = 1')
+  connection.row_factory = dictionary_factory
   return connection
 
 
@@ -36,3 +45,15 @@ def insert_match(connection, match):
              VALUES (:id, :team0, :team1, :score0, :score1, :date)'''
     connection.execute(sql, match)
     connection.commit()
+
+
+def teams(connection):
+  sql = 'SELECT * FROM teams'
+  cursor = connection.execute(sql)
+  return cursor.fetchall()
+
+
+def matches(connection):
+    sql = 'SELECT * FROM matches'
+    cursor = connection.execute(sql)
+    return cursor.fetchall()
