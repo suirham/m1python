c518157f24b0a321ce2bbe1983bfdab015322690
Bertrand Estellon <bertrand.estellon@univ-amu.fr>
2024-09-20 09:58:33 +0200

    TD2

--- flask_app/model.py
+++ flask_app/model.py
@@ -36,24 +36,72 @@ def create_database(connection):
 
 def insert_team(connection, team):
   sql = 'INSERT INTO teams (id, name) VALUES (:id, :name)'
-  connection.execute(sql, team)
+  connection.execute(sql, team)  
   connection.commit()
 
 
 def insert_match(connection, match):
     sql = '''INSERT INTO matches (id, team0, team1, score0, score1, date) 
              VALUES (:id, :team0, :team1, :score0, :score1, :date)'''
-    connection.execute(sql, match)
+    connection.execute(sql, match) 
     connection.commit()
 
 
 def teams(connection):
-  sql = 'SELECT * FROM teams'
+  sql = 'SELECT * FROM teams ORDER BY id'
   cursor = connection.execute(sql)
   return cursor.fetchall()
 
 
 def matches(connection):
-    sql = 'SELECT * FROM matches'
+    sql = 'SELECT * FROM matches ORDER BY id'
     cursor = connection.execute(sql)
     return cursor.fetchall()
+
+
+def fill_database(connection):
+  teams = data.teams()
+  for team in teams:
+    insert_team(connection, team)
+  matches = data.matches()
+  for match in matches:
+    insert_match(connection, match)
+
+
+def update_ranking(connection):
+  sql = "DELETE FROM ranking"
+  connection.execute(sql)
+  teams_ = teams(connection)
+  matches_ = matches(connection)
+  ranking_ = ranking.sorted_ranking(teams_, matches_)
+  sql = '''INSERT INTO ranking(rank, team_id, match_played_count, 
+                                won_match_count, lost_match_count, draw_count, 
+                                goal_for_count, goal_against_count, 
+                                goal_difference, points)
+            VALUES (:rank, :team_id, :match_played_count, 
+                    :won_match_count, :lost_match_count, :draw_count, 
+                    :goal_for_count, :goal_against_count, 
+                    :goal_difference, :points)'''
+  connection.executemany(sql, ranking_)
+  # for row in ranking_: connection.execute(sql, row)
+  connection.commit()
+
+
+def sorted_ranking(connection):
+  sql = '''SELECT ranking.*, teams.name FROM ranking 
+           JOIN teams ON ranking.team_id = teams.id
+           ORDER BY rank'''
+  cursor = connection.execute(sql)
+  return cursor.fetchall()
+
+
+def team_matches(connection, team_id):
+  sql = '''
+    SELECT matches.*, t0.name AS name0, t1.name AS name1 FROM matches
+    JOIN teams AS t0 ON matches.team0 = t0.id
+    JOIN teams AS t1 ON matches.team1 = t1.id
+    WHERE matches.team0 = :team_id OR matches.team1 = :team_id
+    ORDER BY date
+  '''
+  cursor = connection.execute(sql, {'team_id': team_id})
+  return cursor.fetchall()
---
+++ flask_app/test_model.py
@@ -0,0 +1,56 @@
+from flask_app import model
+from flask_app import data
+
+def test_teams_and_insert_team():
+    connection = model.connect(":memory:")
+    model.create_database(connection)
+    teams = data.teams()
+    model.insert_team(connection, teams[4])
+    model.insert_team(connection, teams[2])
+    assert model.teams(connection) == [teams[2], teams[4]]
+
+
+def test_matches_and_insert_match():
+    connection = model.connect(":memory:")
+    model.create_database(connection)
+    teams = data.teams()
+    matches = data.matches()
+    model.insert_team(connection, teams[7])
+    model.insert_team(connection, teams[19])
+    model.insert_match(connection, matches[194])
+    model.insert_match(connection, matches[4])
+    assert model.matches(connection) == [matches[4], matches[194]]
+
+
+def test_fill_database():
+    connection = model.connect(":memory:")
+    model.create_database(connection)
+    model.fill_database(connection)
+    assert model.teams(connection) == data.teams()
+    assert model.matches(connection) == data.matches()
+
+
+def test_update_ranking():
+  connection = model.connect(":memory:")
+  model.create_database(connection)
+  model.update_ranking(connection)
+  model.fill_database(connection)
+  model.update_ranking(connection)
+  model.update_ranking(connection)
+  cursor = connection.execute("SELECT * from ranking ORDER BY rank")
+  assert list(cursor) == data.expected_sorted_ranking()
+
+
+def test_sorted_ranking():
+  connection = model.connect(":memory:")
+  model.create_database(connection)
+  model.fill_database(connection)
+  model.update_ranking(connection)
+  assert model.sorted_ranking(connection) == data.expected_sorted_ranking_with_name()
+
+
+def test_team_matches():
+  connection = model.connect(":memory:")
+  model.create_database(connection)
+  model.fill_database(connection)
+  assert model.team_matches(connection, 4) == data.expected_team_matches_for_team_4()
